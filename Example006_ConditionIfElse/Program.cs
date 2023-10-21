dd6fb0416fcdbfcb352c76c380624e206e4024a0
safronofff <yuriy.safronoff@yandex.ru>
2023-10-21 15:55:26 +0300

    if else

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+
+if (username.ToLower()=="маша")
+{
+    Console.WriteLine("Это же Маша!");
+}
+else
+{
+    Console.WriteLine($"Привет {username}");
+}
