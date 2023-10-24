6fd6f7f61d6273347cdc744dd6bf6a8b0acef41a
safronofff <yuriy.safronoff@yandex.ru>
2023-10-24 20:27:33 +0300

    method array

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {13,45,76,33,61,12,60,33,20,15,33};
+
+int n = array.Length;
+int find = 33;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
