73692fe4ddd7a0bdc902b6817db73532fdc36b58
safronofff <yuriy.safronoff@yandex.ru>
2023-10-24 21:44:38 +0300

    array library

---
+++ Example012_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] array = new int[10];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index<length)
+    {
+        collection[index]=new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count=collection.Length;
+    int index=0;
+    int position=0;
+    while(index<count)
+    {
+        if(collection[index]==find)
+        {
+            position=index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+FillArray(array);
+array[4]=4;
+array[6]=4;
+
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos=IndexOf(array,4);
+Console.WriteLine(pos);
