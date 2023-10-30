d7f0e3d48bfadb472216b103179831581e2fd4ad
safronofff <yuriy.safronoff@yandex.ru>
2023-10-31 02:14:47 +0300

    fibonachi

---
+++ Example020_Fibonachi/Program.cs
@@ -0,0 +1,19 @@
+﻿//Fibonachi
+//f(n)=f(n-1)+f(n-2)
+
+double Fibonachi(double n)
+{
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonachi(n - 1) + Fibonachi(n - 2);
+    }
+}
+
+for (double i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f{i} = {Fibonachi(i)}");
+}
