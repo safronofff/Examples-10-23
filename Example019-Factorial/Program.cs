cd41a27c3950bc3824997b210a4dd65cb8ec92c3
safronofff <yuriy.safronoff@yandex.ru>
2023-10-31 02:14:21 +0300

    factorial

---
+++ Example019-Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿double Factorial(double n)
+{
+    // 1!=1
+    // 0!=1
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+for (double i = 1; i < 50; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
