e03723b78b6d60ed45be6a09c767a20171d17385
safronofff <yuriy.safronoff@yandex.ru>
2023-11-06 18:26:25 +0300

    qqq

---
+++ Example021_OtADoB/Program.cs
@@ -0,0 +1,55 @@
+﻿// От меньшего к большему
+
+
+string NumberFor1(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i}";
+    }
+    return result;
+}
+
+string NumberRec1(int a, int b)
+{
+    if (a <= b)
+    {
+        return $"{a}" + NumberRec1(a + 1,b);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
+Console.WriteLine(NumberFor1(1, 10));
+Console.WriteLine(NumberRec1(1, 10));
+
+// От большего к меньшему
+
+string NumberFor2(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = b; i >= a; i--)
+    {
+        result = result + $"{i}";
+    }
+    return result;
+}
+
+
+string NumberRec2(int a, int b)
+{
+    if (a <= b)
+    {
+        return  NumberRec2(a + 1,b) + $"{a}";
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
+Console.WriteLine(NumberFor2(1, 10));
+Console.WriteLine(NumberRec2(1, 10));
