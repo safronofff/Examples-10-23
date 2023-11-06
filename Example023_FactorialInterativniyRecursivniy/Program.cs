db701837244e183d553690a6ebc9e314b4557ce7
safronofff <yuriy.safronoff@yandex.ru>
2023-11-06 19:40:24 +0300

    fibonachi

---
+++ Example023_FactorialInterativniyRecursivniy/Program.cs
@@ -0,0 +1,26 @@
+﻿// Факториал числа Интеративным и рекурсивным методом
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int FactorialRec(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * FactorialRec(n - 1);
+    }
+}
+
+Console.WriteLine(FactorialFor(10));
+Console.WriteLine(FactorialRec(10));
