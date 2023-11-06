47cdd9e9fd3c85f1f6681debcc55a38abc01a73b
safronofff <yuriy.safronoff@yandex.ru>
2023-11-06 20:27:59 +0300

    asd

---
+++ Example024_AVStepeniN/Program.cs
@@ -0,0 +1,31 @@
+﻿// Вычислить А в степени N
+
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int PowerRec(int a, int n)
+{
+    //return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+    if (n == 0)
+    {
+        return 1;
+    }
+    /*else if (n == 1) //вызывает ошибку и считает на одну рекурсию меньше
+    {
+        return 1;
+    }*/
+    else
+    {
+        return PowerRec(a, n - 1) * a;
+    }
+}
+
+Console.WriteLine(PowerFor(2, 10));
+Console.WriteLine(PowerRec(2, 10));
